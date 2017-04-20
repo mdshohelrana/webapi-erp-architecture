96cfefb430c907fb33f7c5b732bf171ebd9ccd6d
“[shohel <[cse_shohel@yahoo.com]>
2017-04-20 21:34:15 +0600

    Basic Setup

---
+++ EMS.Infra.CrossCutting.Log/ActionFilters/LoggingFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using System.Web.Http.Tracing;
+using System.Web.Http;
+
+
+namespace EMS.Infra.CrossCutting.Log.ActionFilters
+{
+    public class LoggingFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext filterContext)
+        {
+            GlobalConfiguration.Configuration.Services.Replace(typeof(ITraceWriter), new NLogger());
+            var trace = GlobalConfiguration.Configuration.Services.GetTraceWriter();
+            trace.Info(filterContext.Request, "Controller : " + filterContext.ControllerContext.ControllerDescriptor.ControllerType.FullName + Environment.NewLine + "Action : " + filterContext.ActionDescriptor.ActionName, "JSON", filterContext.ActionArguments);
+        }
+    }
+}
