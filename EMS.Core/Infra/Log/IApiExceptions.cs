b0c0cdc1d6172160aa958586574d1dd300692f1c
“[shohel <[cse_shohel@yahoo.com]>
2017-04-20 22:11:26 +0600

    basic setup

---
+++ EMS.Core/Infra/Log/IApiExceptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace EMS.Core.Infra.Log
+{
+    /// <summary>
+    /// IApiExceptions Interface
+    /// </summary>
+    public interface IApiExceptions
+    {
+        /// <summary>
+        /// ErrorCode
+        /// </summary>
+        int ErrorCode { get; set; }
+
+        /// <summary>
+        /// ErrorDescription
+        /// </summary>
+        string ErrorDescription { get; set; }
+
+        /// <summary>
+        /// HttpStatus
+        /// </summary>
+        HttpStatusCode HttpStatus { get; set; }
+
+        /// <summary>
+        /// ReasonPhrase
+        /// </summary>
+        string ReasonPhrase { get; set; }
+    }
+}
